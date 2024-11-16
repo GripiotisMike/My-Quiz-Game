3e015032b1f34b16d3892de0e6a843c9bfebda39
GripiotisMike <gripiotism@yahoo.com>
2024-11-16 17:06:08 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,15 +2,13 @@ from question_model import Question
 from data import question_data
 from quiz_brain import QuizBrain
 
-question_bank = []
-for question in question_data:
-    q_t = question["question"]
-    q_a = question["correct_answer"]
-    n_q = Question(q_t, q_a)
-    question_bank.append(n_q)
+# Create question bank using list comprehension
+question_bank = [Question(q["question"], q["correct_answer"]) for q in question_data]
 
+# Initialize the quiz and start asking questions
 quiz = QuizBrain(question_bank)
-while quiz.still_has_questions:
+
+while quiz.still_has_questions():
     quiz.next_question()
 
 print("You have completed the quiz!")
